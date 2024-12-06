faa1ebe2673dfc3a625892209b02345c7f89a94d
samchi <seyoung0314@gmail.com>
2024-12-06 17:44:15 +0900

    자바 컬렉션 프레임워크 연습예제(선생님버전)

---
+++ src/chap2_4/song_ex/Run.java
@@ -0,0 +1,12 @@
+package chap2_4.song_ex;
+
+import chap2_4.song_ex.ArtistController;
+
+public class Run {
+
+    public static void main(String[] args) {
+
+        ArtistController controller = new ArtistController();
+        controller.start();
+    }
+}
